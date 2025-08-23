cf4907d67fa21c534fdd28c99b0a363525112ad6
KXand <71508678+KXAND@users.noreply.github.com>
2025-08-23 17:46:06 +0800

    添加了changes文件，优化translator并翻译quickstrings

--- translator.py
+++ translator.py
@@ -11,8 +11,8 @@ from key import _API_KEY
 # === 1. 配置 ===
 API_KEY = _API_KEY
 BASE_URL = "https://api.siliconflow.cn/v1"
-TRANSLATE_MODEL_NAME = "Qwen/Qwen2.5-72B-Instruct-128K"  # 模型名
-PROOFREAD_MODEL_NAME = "Qwen/Qwen3-8B"  # 模型名
+TRANSLATE_MODEL = "moonshotai/Kimi-K2-Instruct"  # 模型名
+PROOFREAD_MODEL = "Qwen/Qwen3-8B"  # 模型名
 SOURCE_DIR = "comparation/"
 RESULT_DIR = "Qwen2.5-72B-Instruct-128K/"
 CACHE_FILE = "translation_cache.json"  # 缓存文件
@@ -20,15 +20,15 @@ CACHE_PRF_FILE = "proofread_cache.json"  # 校对缓存文件
 GLOSSARY_FILE = "GLOSSARY.json"  # 缓存文件
 SOURCE_LANGUAGE = "English"
 TARGET_LANGUAGE = "Chinese"
-TARGET_FILES = ["troops", "party_templates"]  # without extension name
+TARGET_FILES = ["quick_strings"]  # without extension name
 BATCH_SIZE = 10  # 每批处理的 Unit 个数
 BATCH_UNIT = 20  # 每次请求包含的行数
 
 # 术语表
 with open(GLOSSARY_FILE, "r", encoding="utf-8") as f:
     GLOSSARY = json.load(f)
-CURRET_SYS_PROMPT = sys_prompt.troop_system_prompt
-
+CURRET_SYS_PROMPT = sys_prompt.sentence_system_prompt
+CURRET_PROOFREAD_SYS_PROMPT = sys_prompt.sentence_proofread_prompt
 # 翻译风格
 STYLE_GUIDE = "formal and concise"
 
@@ -36,9 +36,9 @@ STYLE_GUIDE = "formal and concise"
 client = AsyncOpenAI(api_key=API_KEY, base_url=BASE_URL)
 if os.path.exists(CACHE_FILE):
     with open(CACHE_FILE, "r", encoding="utf-8") as f:
-        cache = json.load(f)
+        translation_cache = json.load(f)
 else:
-    cache = {}
+    translation_cache = {}
 
 if os.path.exists(CACHE_PRF_FILE):
     with open(CACHE_PRF_FILE, "r", encoding="utf-8") as f:
@@ -60,112 +60,107 @@ def get_local_glossary(text_bundle):
 
 
 # === 3. 翻译函数（带缓存） ===
-async def translate_text(text_bundle, glossary=GLOSSARY):
-    start = time.time()
-
+async def translate_text(text_bundle, system_prompt, model, cache, glossary=GLOSSARY):
     user_prompt = ""
+    cnt_sent = 0
     # 剔除已经在缓存中的结果
     for key, text, reference, *_ in text_bundle:
-        refined_text = text.replace("_", " ")
-        if refined_text not in cache:
-            user_prompt += refined_text
-            user_prompt += ("，参考译名: " + reference) if reference else ""
+        cleaned_text = text.replace("_", " ").strip()
+        if cleaned_text not in cache and cleaned_text != "":
+            user_prompt += cleaned_text
+            user_prompt += ("，参考翻译: " + reference) if reference else ""
             user_prompt += "\n"
+            cnt_sent += 1
 
+    # 如果并非都在缓存中，调用AI翻译并写入到 cache 中
     translated = []
-    if user_prompt.strip() == "":
-        for key, text, reference, *_ in text_bundle:
-            clean_text = text.replace("_", " ").strip()
-            translated.append([key, text, cache.get(clean_text)])
-        return translated
+    if user_prompt.strip() != "":
+        # 获取结果
 
-    system_prompt = CURRET_SYS_PROMPT(glossary)
-    response = await client.chat.completions.create(
-        model=TRANSLATE_MODEL_NAME,
-        messages=[
-            {"role": "system", "content": system_prompt.strip()},
-            {
-                "role": "user",
-                "content": f"Source text:\n{user_prompt}",
-            },
-        ],
-        temperature=0,
-    )
+        response = await client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {
+                    "role": "user",
+                    "content": f"\n{user_prompt}",
+                },
+            ],
+            temperature=0,
+            # extra_body={"enable_thinking": False},
+        )
 
-    content = response.choices[0].message.content
-    if content is not None:
-        for translation_pair in content.strip().split("\n"):
+        # 错误情况：结果为空
+        content = response.choices[0].message.content
+        if content is None:
+            return await translate_text(
+                text_bundle, system_prompt, model, cache, glossary
+            )
+
+        # 错误情况：结果数量不匹配
+        translation_pairs = content.replace("\n\n", "\n").strip().split("\n")
+        if len(translation_pairs) != cnt_sent:
+            print(
+                f"翻译数目不匹配。输入： {len(translation_pairs)}   {len(text_bundle)}  {cnt_sent}"
+            )
+            print(translation_pairs)
+            print(text_bundle)
+            return await translate_text(
+                text_bundle, system_prompt, model, cache, glossary
+            )
+
+        # 新翻译塞入 cache
+        i = 0
+        for key, text, _ in text_bundle:
+            cleaned_text = text.replace("_", " ").strip()
+            if cleaned_text in cache or cleaned_text == "":
+                continue
             try:
-                text, translation = translation_pair.strip().split(":")
-                cache[text.strip()] = translation.strip()
+                trans = translation_pairs[i]
+                cache[cleaned_text] = trans
+                i += 1
             except:
-                print("已忽略不正确的返回格式对，内容为：" + translation_pair)
+                i += 1
+                print(
+                    f"已忽略不正确的返回格式对，内容为：{ trans} i={i} cleaned_text={cleaned_text}"
+                )
 
+    # 从 cache 中取出所有的翻译
     for key, text, reference, *_ in text_bundle:
-        clean_text = text.replace("_", " ").strip()
-        translated.append([key, text, cache.get(clean_text)])
-
-    total_time = time.time() - start
-    print(f"translate_text completed in {total_time:.2f}s")
+        cleaned_text = text.replace("_", " ").strip()
+        translated.append([key, text, cache.get(cleaned_text)])
     return translated
 
 
-# === 4.单个请求的校对 ===
-async def proofread_texts(text_bundle, glossary):
-    start = time.time()
-
-    user_prompt = ""
-    # 剔除已经在缓存中的结果
-    for key, text, translated in text_bundle:
-        refined_text = text.replace("_", " ")
-        if refined_text not in proofread_cache:
-            user_prompt += refined_text
-            user_prompt += ("，参考译名: " + translated) if translated else ""
-            user_prompt += "\n"
-
-    translateds = []
-    if user_prompt.strip() == "":
-        for key, text, translated in text_bundle:
-            clean_text = text.replace("_", " ").strip()
-            translateds.append([key, text, proofread_cache.get(clean_text)])
-        return translateds
-
-    resp = await client.chat.completions.create(
-        model=PROOFREAD_MODEL_NAME,
-        messages=[
-            {"role": "system", "content": sys_prompt.proofread_prompt(glossary)},
-            {"role": "user", "content": user_prompt},
-        ],
-        temperature=0,
-        extra_body={"enable_thinking": False},
-    )
-    checked_texts = resp.choices[0].message.content
-
-    result = []
-    if checked_texts is not None:
-        for checked_text in checked_texts.strip().split("\n"):
-            try:
-                text, proofreaded_trans = checked_text.strip().split(":")
-                proofread_cache[text.strip()] = proofreaded_trans.strip()
-            except:
-                print("已忽略不正确的返回格式对，内容为：" + checked_text)
-    for key, text, translated in text_bundle:
-        clean_text = text.replace("_", " ").strip()
-        result.append([key, text, proofread_cache.get(clean_text)])
-
-    total_time = time.time() - start
-    print(f"proofread_texts completed in {total_time:.2f}s")
-    return result
-
-
 async def process_unit(unit):
-    start = time.time()
+    unit_start = time.time()
     glossary = get_local_glossary(unit)
-    translated = await translate_text(unit, glossary)
-    proofreaded = await proofread_texts(translated, glossary)
 
+    start = time.time()
+    translated = await translate_text(
+        unit,
+        CURRET_SYS_PROMPT(glossary),
+        TRANSLATE_MODEL,
+        translation_cache,
+        glossary,
+    )
     total_time = time.time() - start
-    print(f"process_unit completed in {total_time:.2f}s")
+    print(f"translate_text completed in {total_time:.2f}s")
+
+    proofreaded = translated
+    # start = time.time()
+    # proofreaded = await translate_text(
+    #     translated,
+    #     CURRET_PROOFREAD_SYS_PROMPT(glossary),
+    #     PROOFREAD_MODEL,
+    #     proofread_cache,
+    #     glossary,
+    # )
+    # total_time = time.time() - start
+    # print(f"translate_text completed in {total_time:.2f}s")
+
+    unit_total_time = time.time() - unit_start
+    print(f"process_unit completed in {unit_total_time:.2f}s")
     return proofreaded
 
 
@@ -202,7 +197,7 @@ async def main():
 
             # 保存缓存
             with open(CACHE_FILE, "w", encoding="utf-8") as f:
-                json.dump(cache, f, ensure_ascii=False, indent=2)
+                json.dump(translation_cache, f, ensure_ascii=False, indent=2)
             with open(CACHE_PRF_FILE, "w", encoding="utf-8") as f:
                 json.dump(proofread_cache, f, ensure_ascii=False, indent=2)
 
